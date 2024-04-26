6c2ae5a8386b826dbb35096d6d43f3234d48bf5f
Kevin Hurtado <kevinhurtado002@gmail.com>
2024-04-25 20:37:05 -0500

    Update CreateNoteDto with validation decorators

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,16 +1,12 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
-
 export class CreateNoteDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  title: string;
 
-    @IsString()
-    @MinLength(1)
-    @MaxLength(50)
-    title: string;
-
-    @IsString()
-    @MinLength(1)
-    @MaxLength(1000)
-    description: string;
-
+  @IsString()
+  @MinLength(1)
+  description: string;
 }
